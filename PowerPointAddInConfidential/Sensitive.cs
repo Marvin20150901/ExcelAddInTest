3a8a71856a5762a835608521bf952524079b6fc9
woyao <woyao@MARVIN-PC>
2018-07-04 22:08:19 +0800

    Add delete image function

--- PowerPointAddInConfidential/Sensitive.cs
+++ PowerPointAddInConfidential/Sensitive.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 using Microsoft.Office.Core;
+using Microsoft.Office.Interop.PowerPoint;
 using Microsoft.Office.Tools.Ribbon;
+using Shape = Microsoft.Office.Interop.PowerPoint.Shape;
 
 namespace PowerPointAddInConfidential
 {
@@ -23,13 +27,37 @@ namespace PowerPointAddInConfidential
             var size = Properties.Resources.Secret.Size;
             if (Properties.Settings.Default.IsMask)
             {
-                string logo = "Secret.png";
+                foreach (Shape slideMasterShape in activePpt.SlideMaster.Shapes)
+                {
+                    if (slideMasterShape.Name.Equals(@"{BE32D37E-C6BF-42CF-A306-46B34A2066FF}"))
+                    {
+                        slideMasterShape.Delete();
+                    }
+                }
+
+                if (!File.Exists("Secret.png"))
+                {
+                    Properties.Resources.Secret.Save("Secret.png");
+                }
+
+                string logo = @"Secret.png";
                 var shap=activePpt.SlideMaster.Shapes.AddPicture(logo, MsoTriState.msoFalse, MsoTriState.msoTrue, 0, 0, size.Width,size.Height);
                 shap.Line.Visible=MsoTriState.msoFalse;
+                shap.Name = @"{BE32D37E-C6BF-42CF-A306-46B34A2066FF}";
             }
             else
             {
 
+                 //activePpt.SlideMaster.Shapes
+                foreach (Shape slideMasterShape in activePpt.SlideMaster.Shapes)
+                {
+                    if (slideMasterShape.Name.Equals(@"{BE32D37E-C6BF-42CF-A306-46B34A2066FF}"))
+                    {
+                       // MessageBox.Show("heheheeh");
+                        slideMasterShape.Delete();
+                    }
+                    //slideMasterShape.
+                }
             }
 
 
@@ -40,15 +68,39 @@ namespace PowerPointAddInConfidential
             SetWorkbookSensitive("Confidential");
 
             var activePpt = Globals.ThisAddIn.Application.ActivePresentation;
-
+            var size = Properties.Resources.Confidential.Size;
             if (Properties.Settings.Default.IsMask)
             {
+                foreach (Shape slideMasterShape in activePpt.SlideMaster.Shapes)
+                {
+                    if (slideMasterShape.Name.Equals(@"{BE32D37E-C6BF-42CF-A306-46B34A2066FF}"))
+                    {
+                        slideMasterShape.Delete();
+                    }
+                }
 
+                if (!File.Exists("Confidential.png"))
+                {
+                    Properties.Resources.Confidential.Save("Confidential.png");
+                }
+
+                string logo = @"Confidential.png";
+                var shap = activePpt.SlideMaster.Shapes.AddPicture(logo, MsoTriState.msoFalse, MsoTriState.msoTrue, 0, 0, size.Width, size.Height);
+                shap.Line.Visible = MsoTriState.msoFalse;
+                shap.Name = @"{BE32D37E-C6BF-42CF-A306-46B34A2066FF}";
                 //add  logic
             }
             else
             {
-
+                foreach (Shape slideMasterShape in activePpt.SlideMaster.Shapes)
+                {
+                    if (slideMasterShape.Name.Equals(@"{BE32D37E-C6BF-42CF-A306-46B34A2066FF}"))
+                    {
+                        // MessageBox.Show("heheheeh");
+                        slideMasterShape.Delete();
+                    }
+                    //slideMasterShape.
+                }
             }
 
         }
@@ -89,13 +141,24 @@ namespace PowerPointAddInConfidential
 
         private void toggleButtonMarkYes_Click(object sender, RibbonControlEventArgs e)
         {
+            Globals.Ribbons.Sensitive.toggleButtonMarkYes.Checked = true;
+            Globals.Ribbons.Sensitive.toggleButtonMarkNo.Checked = false;
 
+            Properties.Settings.Default.IsMask = true;
+            Properties.Settings.Default.Save();
         }
 
         private void toggleButtonMarkNo_Click(object sender, RibbonControlEventArgs e)
         {
+            Globals.Ribbons.Sensitive.toggleButtonMarkYes.Checked = false;
+            Globals.Ribbons.Sensitive.toggleButtonMarkNo.Checked = true;
 
+
+            Properties.Settings.Default.IsMask = false;
+            Properties.Settings.Default.Save();
         }
+
+
 
 
 
